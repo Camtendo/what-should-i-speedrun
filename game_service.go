9033a08e24be856bbaa0020e105cebc066a1e8c0
Cameron Crockrom <ccrockrom@gmail.com>
2014-02-20 16:13:21 -0600

    Deserialized gamelist into usable model.

--- game_service.go
+++ game_service.go
@@ -7,31 +7,21 @@ import (
 	"os"
 )
 
-type jsonobject struct {
-    Object ObjectType
-}
- 
-type ObjectType struct {
-    Buffer_size int
-    Games []Game
-}
-
 type Game struct {
-	Title string
-	System string
-	SDATime string
+	Title string `json:"Title"`
+	Console string `json:"System"`
+	Time string `json:"SDA_Time"`
 }
 
 func main() {
-    //Read in gamelist
+	//Read in gamelist
     file, e := ioutil.ReadFile("gamelist.json")
     if e != nil {
     	fmt.Printf("There was an error reading the file! %v\n", e)
     	os.Exit(1)
     }
-    fmt.Printf("%s\n", string(file))
 
-    var jsontype jsonobject
-    json.Unmarshal(file, &jsontype)
-    fmt.Printf("Results: %v\n", jsontype)
+    var games []Game
+    json.Unmarshal(file, &games)
+    fmt.Println(games[4].Console)
 }
